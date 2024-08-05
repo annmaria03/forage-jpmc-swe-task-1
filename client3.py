efe4ec9d66b364c32ea9d7f2a3ab9cd37a75ecf5
Ann Maria Sebastian <annmariasen322@gmail.com>
2024-08-05 12:51:53 +0530

    Fixed stock price calculation and ratio computation in client3.py

--- client3.py
+++ client3.py
@@ -28,32 +28,34 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Compute the average price
     return stock, bid_price, ask_price, price
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
-
+    if price_b == 0:
+        return float('inf')  # Handle division by zero
+    return price_a / price_b
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    prices = {}
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price  # Store the price in the dictionary
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        if len(prices) == 2:
+            stock_names = list(prices.keys())
+            price_a = prices[stock_names[0]]
+            price_b = prices[stock_names[1]]
+            print("Ratio %s" % getRatio(price_a, price_b))
